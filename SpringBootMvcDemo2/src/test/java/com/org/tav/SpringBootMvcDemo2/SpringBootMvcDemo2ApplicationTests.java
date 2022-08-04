0527d028a9b6c79051c0d51a9eadd4eb6113b45b
Dheerajkandikattu <65445438+Dheerajkandikattu@users.noreply.github.com>
2022-08-04 12:40:08 +0530

    Add files via upload

---
+++ SpringBootMvcDemo2/src/test/java/com/org/tav/SpringBootMvcDemo2/SpringBootMvcDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.tav.SpringBootMvcDemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
